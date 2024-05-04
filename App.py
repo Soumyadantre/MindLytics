32d9aae08be67f2e3692d594016944b611b8b83c
Soumya <132812436+Soumyadantre@users.noreply.github.com>
2024-05-04 23:06:53 +0530

    Update App.py

--- App.py
+++ App.py
@@ -8,27 +8,27 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 app = Flask(__name__)
 
 # Load the pre-trained model
-model = load_model('mental_health_model.h5')
+model = load_model('models/mental_health_model.h5')
 
 # Load the tokenizer
 tokenizer = Tokenizer()
-tokenizer.fit_on_texts(["How often do you feel sad or hopeless?", 
-                        "Do you have trouble concentrating on things like reading or watching TV?",
-                        "How often do you have trouble sleeping?",
-                        "Do you often feel worried or on edge?",
-                        "Do you ever have thoughts of harming yourself?",
-                        "How often do you feel excessively happy or 'high'?",
-                        "Do you frequently experience sudden and intense fear or panic?",
-                        "How often do you experience intrusive thoughts or urges?",
-                        "Do you frequently feel detached from reality or experience hallucinations?",
-                        "How often do you have trouble controlling your temper?",
-                        "Do you often have trouble remembering things?",
-                        "Do you feel like you have little energy or feel tired all the time?"])
+tokenizer.fit_on_texts(["How often do you feel sad or hopeless?\nA) Almost every day\nB) Rarely\nC) Never",
+                        "Do you have trouble concentrating on things like reading or watching TV?\nA) Yes\nB) No",
+                        "How often do you have trouble sleeping?\nA) Almost every night\nB) Occasionally\nC) Never",
+                        "Do you often feel worried or on edge?\nA) Yes\nB) No",
+                        "Do you ever have thoughts of harming yourself?\nA) Yes\nB) No",
+                        "How often do you feel excessively happy or 'high'?\nA) Almost every day\nB) Rarely\nC) Never",
+                        "Do you frequently experience sudden and intense fear or panic?\nA) Yes\nB) No",
+                        "How often do you experience intrusive thoughts or urges?\nA) Several times a day\nB) Occasionally\nC) Never",
+                        "Do you frequently feel detached from reality or experience hallucinations?\nA) Yes\nB) No",
+                        "How often do you have trouble controlling your temper?\nA) Almost every day\nB) Rarely\nC) Never",
+                        "Do you often have trouble remembering things?\nA) Yes\nB) No",
+                        "Do you feel like you have little energy or feel tired all the time?\nA) Yes\nB) No"])
 
 # Routes
 @app.route('/')
 def home():
-    questions = tokenizer.word_index.keys()
+    questions = list(tokenizer.word_index.keys())
     return render_template('index.html', questions=questions)
 
 @app.route('/predict', methods=['POST'])
@@ -44,10 +44,11 @@ def predict():
         
         # Predict the mental health issue
         prediction = model.predict(user_padded)
-        predicted_label = np.argmax(prediction)
-        predicted_issue = label_encoder.classes_[predicted_label]
+        predicted_labels = np.argmax(prediction, axis=1)
+        predicted_issues = [tokenizer.index_word[label] for label in predicted_labels]
         
-        return render_template('result.html', predicted_issue=predicted_issue)
+        return render_template('result.html', predicted_issues=predicted_issues)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
